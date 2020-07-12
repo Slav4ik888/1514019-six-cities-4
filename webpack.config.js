826d2f9dd1d00b38ae34b1bcb85c9842cb1f6e3b
Vyacheslav <korzan.va@mail.ru>
2020-07-13 00:04:31 +0800

    Вернул порт 1337

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, `public`),
     open: false,
-    port: 3010,
+    port: 1337,
     historyApiFallback: true,
   },
   module: {
