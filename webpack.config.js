dad2d36cd73db0d8b79fc09e66dc18c30b1870a1
Vyacheslav <korzan.va@mail.ru>
2020-07-17 18:56:42 +0800

    вернул1337

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     // eslint-disable-next-line no-undef
     contentBase: path.join(__dirname, `public`),
     open: false,
-    port: 3010,
+    port: 1337,
     historyApiFallback: true,
   },
   module: {
