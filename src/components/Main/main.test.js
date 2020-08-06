c4227ac80a6cae0adab6c42f19abbe1675333b60
Vyacheslav <korzan.va@mail.ru>
2020-08-06 17:56:48 +0800

    refact test Main

--- src/components/Main/main.test.js
+++ src/components/Main/main.test.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 import Main from './main.jsx';
 
 import {NameSpace} from '../../reducers/name-space.js';
-// import {offers} from "../../mocks/offers.js";
+import {offers} from "../../mocks/offers.js";
 import {testOffer} from '../../mocks/test-offer.js';
 
 
@@ -23,48 +23,48 @@ const authInfo = {
 
 
 describe(`Snapshot of <Main/>`, () => {
-  // it(`Render <Main/> with offers`, () => {
-  //   const store = mockStore({
-  //     [NameSpace.TRAVEL]: {
-  //       activeCity: 0,
-  //       activeOffer: null,
-  //       activeHoverOffer: testOffer,
-  //     },
-  //     [NameSpace.USER]: {
-  //       userStatus: `AUTH`,
-  //       authInfo,
-  //       isLoading: false,
-  //     },
-  //     [NameSpace.DATA]: {
-  //       allOffers: offers,
-  //       comments: [],
-  //       nearbyOffers: [],
-  //       isLoading: false,
-  //     },
-  //   });
+  it(`Render <Main/> with offers`, () => {
+    const store = mockStore({
+      [NameSpace.TRAVEL]: {
+        activeCity: 0,
+        activeOffer: null,
+        activeHoverOffer: testOffer,
+      },
+      [NameSpace.USER]: {
+        userStatus: `AUTH`,
+        authInfo,
+        isLoading: false,
+      },
+      [NameSpace.DATA]: {
+        allOffers: offers,
+        comments: [],
+        nearbyOffers: [],
+        isLoading: false,
+      },
+    });
 
-  //   const tree = renderer
-  //     .create(
-  //         <Provider store={store}>
-  //           <BrowserRouter>
-  //             <Main
-  //               isLoading={false}
-  //               offers={offers.Paris}
-  //               activeCity={0}
-  //               activeHoverOffer={testOffer}
-  //               handleCardTitleClick={() => {}}
-  //               handleChangeCity={() => {}}
-  //             />
-  //           </BrowserRouter>
-  //         </Provider>, {
-  //           createNodeMock: () => {
-  //             return {};
-  //           }
-  //         }
-  //     ).toJSON();
+    const tree = renderer
+      .create(
+          <Provider store={store}>
+            <BrowserRouter>
+              <Main
+                isLoading={false}
+                offers={offers.Paris}
+                activeCity={0}
+                activeHoverOffer={testOffer}
+                handleCardTitleClick={() => {}}
+                handleChangeCity={() => {}}
+              />
+            </BrowserRouter>
+          </Provider>, {
+            createNodeMock: () => {
+              return {};
+            }
+          }
+      ).toJSON();
 
-  //   expect(tree).toMatchSnapshot();
-  // });
+    expect(tree).toMatchSnapshot();
+  });
 
   it(`Render <Main/> with offers=[]`, () => {
     const store = mockStore({
